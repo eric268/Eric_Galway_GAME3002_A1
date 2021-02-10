9aa722fbf65187f7995795ca4fb50c4684b0f2c9
eric268 <46507613+eric268@users.noreply.github.com>
2021-02-10 13:09:32 -0500

    Added reset scene event

--- Assets/Scrips/ArrowDirection.cs
+++ Assets/Scrips/ArrowDirection.cs
@@ -6,12 +6,33 @@ public class ArrowDirection : MonoBehaviour
 {
     private float m_fRotationSpeed = 0.05f;
     private float m_fRotationLimit = 0.0f;
+    private float m_fKickingStrength = 1.0f;
     private bool m_bTurnLeft = false;
     private bool m_bTurnRight = false;
     private bool m_bRotateUp = false;
     private bool m_bRotateDown = false;
-    
+    private bool m_bChargeKickStrength = false;
+    private bool m_bBallKicked = false;
+
+    private bool m_bDebugDirection = false;
+
+    private Vector3 m_vDirection = Vector3.zero;
+    private Vector3 m_vStartingPosition = Vector3.zero;
+    private Quaternion m_qStartRotation = Quaternion.identity;
+
+
+
+    private Vector3 m_vStartingArrowPosition = Vector3.zero;
+    private Vector3 m_vStartingArrow2Position = Vector3.zero;
+    private Vector3 m_vStartingArrowScale = Vector3.zero;
+    private Vector3 m_vStartingArrow2Scale = Vector3.zero; 
+    private Quaternion m_qArrowRotation = Quaternion.identity;
+    private Quaternion m_qArrow2Rotation = Quaternion.identity;
+
     GameObject m_arrow;
+    GameObject m_soccerBall;
+    GameObject m_arrowBox;
+    GameObject m_arrowBox2;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +42,41 @@ public class ArrowDirection : MonoBehaviour
         {
             Debug.LogError("Can't find arrow");
         }
-        
+
+        m_soccerBall = GameObject.Find("Soccer Ball");
+        if (!m_soccerBall)
+        {
+            Debug.LogError("Can't find soccer ball");
+        }
+
+        m_arrowBox = GameObject.Find("Box");
+        if (!m_arrowBox)
+        {
+            Debug.LogError("Can't find first part of arrow");
+        }
+
+        m_arrowBox2 = GameObject.Find("Box1");
+        if (!m_arrowBox2)
+        {
+            Debug.LogError("Can't find 2nd part of arrow");
+        }
+
+
+        m_qStartRotation = transform.rotation;
+        m_vStartingPosition = transform.position;
+
+        m_vStartingArrowPosition = m_arrowBox.transform.position;
+        m_vStartingArrow2Position = m_arrowBox2.transform.position;
+
+        m_vStartingArrowScale = m_arrowBox.transform.localScale;
+        m_vStartingArrow2Scale = m_arrowBox2.transform.localScale;
+
+        m_qArrowRotation = m_arrowBox.transform.rotation;
+        m_qArrow2Rotation = m_arrowBox2.transform.rotation;
+
+        m_arrowBox.GetComponent<Renderer>().material.color = Color.grey;
+        m_arrowBox2.GetComponent<Renderer>().material.color = Color.grey;
+
     }
 
     // Update is called once per frame
@@ -29,7 +84,7 @@ public class ArrowDirection : MonoBehaviour
     {
         HandleEvents();
         Move();
-        //m_arrow.transform.position += transform.forward;
+
     }
     private void HandleEvents()
     {
@@ -50,6 +105,10 @@ public class ArrowDirection : MonoBehaviour
         {
             m_bRotateDown = true;
         }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            m_bChargeKickStrength = true;
+        }
 
         //Keyup
         if (Input.GetKeyUp(KeyCode.A))
@@ -68,6 +127,24 @@ public class ArrowDirection : MonoBehaviour
         {
             m_bRotateDown = false;
         }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            if (!m_bBallKicked)
+            { 
+            m_vDirection = new Vector3(transform.forward.x, m_arrow.transform.forward.y, transform.forward.z);
+            m_soccerBall.GetComponent<BallPhysics>().m_rb.AddForce(m_vDirection * m_fKickingStrength, ForceMode.Impulse);
+            m_bChargeKickStrength = false;
+            m_bBallKicked = true;
+            m_fKickingStrength = 1.0f;
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            Debug.LogError("R Pressed");
+            ResetSceneObjects();
+
+        }   
+
     }
 
     private void RotateLeft(float speed)
@@ -90,34 +167,104 @@ public class ArrowDirection : MonoBehaviour
 
     private void Move()
     {
-        Debug.Log("rotation limit:" + m_fRotationLimit);
-        if (m_bTurnLeft)
+        //if (!m_bBallKicked)
         {
-            RotateLeft(m_fRotationSpeed);
-        }
-        if (m_bTurnRight)
-        {
-            RotateRight(m_fRotationSpeed);
-        }
-        if (m_bRotateUp)
-        {
-            m_fRotationLimit -= m_fRotationSpeed;
-            m_fRotationLimit = Mathf.Clamp(m_fRotationLimit, -30.0f, 5.0f);
+            if (m_bTurnLeft)
+            {
+                RotateLeft(m_fRotationSpeed);
+            }
+            if (m_bTurnRight)
+            {
+                RotateRight(m_fRotationSpeed);
+            }
+            if (m_bRotateUp)
+            {
+                m_fRotationLimit -= m_fRotationSpeed;
+                m_fRotationLimit = Mathf.Clamp(m_fRotationLimit, -30.0f, 5.0f);
 
-            if (m_fRotationLimit > -30.0f)
-                RotateUp(m_fRotationSpeed);
+                if (m_fRotationLimit > -30.0f)
+                    RotateUp(m_fRotationSpeed);
 
-        }
-        if (m_bRotateDown)
-        {
-            m_fRotationLimit += m_fRotationSpeed;
-            m_fRotationLimit = Mathf.Clamp(m_fRotationLimit, -30.0f, 5.0f);
+            }
+            if (m_bRotateDown)
+            {
+                m_fRotationLimit += m_fRotationSpeed;
+                m_fRotationLimit = Mathf.Clamp(m_fRotationLimit, -30.0f, 5.0f);
 
-            if (m_fRotationLimit < 5.0f)
-            RotateDown(m_fRotationSpeed);
+                if (m_fRotationLimit < 5.0f)
+                    RotateDown(m_fRotationSpeed);
+            }
+
+            if (m_bChargeKickStrength)
+            {
+                Debug.LogError(m_fKickingStrength);
+                m_fKickingStrength += 0.03f;
+                m_fKickingStrength = Mathf.Clamp(m_fKickingStrength, 1.0f, 50.0f);
+                UpdateArrowSizeandColor();
+            }
         }
 
     }
+
+    private void ResetSceneObjects()
+    {
+        transform.rotation = m_qStartRotation;
+        m_soccerBall.transform.position = m_vStartingPosition;
+        m_soccerBall.GetComponent<BallPhysics>().m_rb.velocity = Vector3.zero;
+        m_soccerBall.GetComponent<BallPhysics>().m_rb.angularVelocity = Vector3.zero;
+
+        m_arrowBox.transform.localScale = m_vStartingArrowScale;
+        m_arrowBox2.transform.localScale = m_vStartingArrow2Scale;
+
+        m_arrowBox.transform.position = m_vStartingArrowPosition;
+        m_arrowBox2.transform.position = m_vStartingArrow2Position;
+
+        m_arrowBox.transform.rotation = m_qArrowRotation;
+        m_arrowBox2.transform.rotation = m_qArrow2Rotation;
+
+        m_arrowBox.GetComponent<Renderer>().material.color = Color.grey;
+        m_arrowBox2.GetComponent<Renderer>().material.color = Color.grey;
+
+        ResetSceneVariables();
+
+    }
+
+    private void ResetSceneVariables()
+    {
+        m_bTurnLeft = false;
+        m_bTurnRight = false;
+        m_bRotateUp = false;
+        m_bRotateDown = false;
+        m_bChargeKickStrength = false;
+        m_bBallKicked = false;
+
+        m_fRotationLimit = 0.0f;
+    }
+    private void UpdateArrowSizeandColor()
+    {
+        if (!m_bBallKicked)
+        {
+            if (m_fKickingStrength < 50.0f)
+            {
+                m_arrowBox.transform.localScale += new Vector3(0.0001f, 0.0f, 0.0008f);
+                m_arrowBox2.transform.localScale += new Vector3(0.00005f, 0.0f, 0.0005f);
+                m_arrowBox.transform.position += (transform.forward * 0.0015f);
+                m_arrowBox2.transform.position += (transform.forward * 0.001f);
+            }
+
+            if (m_fKickingStrength > 20.0f && m_fKickingStrength <= 35.0f)
+            {
+                m_arrowBox.GetComponent<Renderer>().material.color = Color.yellow;
+                m_arrowBox2.GetComponent<Renderer>().material.color = Color.yellow;
+            }
+            else if (m_fKickingStrength > 35.0f)
+            {
+                m_arrowBox.GetComponent<Renderer>().material.color = Color.red;
+                m_arrowBox2.GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
+    }
+    
     private void OnDrawGizmos()
     {
         
--- Assets/Scrips/BallPhysics.cs
+++ Assets/Scrips/BallPhysics.cs
@@ -5,22 +5,27 @@ using UnityEngine;
 
 public class BallPhysics : MonoBehaviour
 {
-    private float m_fKickStrength = 1.0f;
-    private float m_fTheta = 1.0f;
-    private Vector3 m_vDirection = Vector3.zero;
-    private Rigidbody m_rb = null;
+
+    private Vector3 m_vPreviousVelocity = Vector3.zero;
+    public Rigidbody m_rb = null;
+    private float m_fSpeed = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         m_rb = GetComponent<Rigidbody>();
         Assert.IsNotNull(m_rb, "No rigid body found on ball");
-        m_vDirection = transform.forward;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        m_vPreviousVelocity = m_rb.velocity;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        float m_fSpeed = m_vPreviousVelocity.magnitude;
+        var direction = Vector3.Reflect(m_vPreviousVelocity.normalized, collision.contacts[0].normal);
+        m_rb.velocity = (direction * Mathf.Max(m_fSpeed, 0f)) * 0.5f;
     }
 }
