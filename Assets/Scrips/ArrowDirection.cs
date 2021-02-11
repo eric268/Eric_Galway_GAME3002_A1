d8a49d7313f3a343e8d5c1787ee91592b111099d
eric268 <46507613+eric268@users.noreply.github.com>
2021-02-11 17:02:02 -0500

    Added projectile motion on x,z axis

--- Assets/Scrips/ArrowDirection.cs
+++ Assets/Scrips/ArrowDirection.cs
@@ -7,8 +7,11 @@ public class ArrowDirection : MonoBehaviour
     private float m_fRotationSpeed = 0.05f;
     private float m_fRotationLimit = 0.0f;
     private float m_fKickingStrength = 1.0f;
+
     private bool m_bTurnLeft = false;
     private bool m_bTurnRight = false;
+    private bool m_bMoveForward = false;
+    private bool m_bMoveBackwards = false;
     private bool m_bRotateUp = false;
     private bool m_bRotateDown = false;
     private bool m_bChargeKickStrength = false;
@@ -18,30 +21,19 @@ public class ArrowDirection : MonoBehaviour
 
     private Vector3 m_vDirection = Vector3.zero;
     private Vector3 m_vStartingPosition = Vector3.zero;
+    [SerializeField]
+    private Vector3 m_vInitialVelocity = Vector3.zero;
     private Quaternion m_qStartRotation = Quaternion.identity;
 
 
 
-    private Vector3 m_vStartingArrowPosition = Vector3.zero;
-    private Vector3 m_vStartingArrow2Position = Vector3.zero;
-    private Vector3 m_vStartingArrowScale = Vector3.zero;
-    private Vector3 m_vStartingArrow2Scale = Vector3.zero; 
-    private Quaternion m_qArrowRotation = Quaternion.identity;
-    private Quaternion m_qArrow2Rotation = Quaternion.identity;
-
-    GameObject m_arrow;
-    GameObject m_soccerBall;
-    GameObject m_arrowBox;
-    GameObject m_arrowBox2;
+    private GameObject m_soccerBall = null;
+    private GameObject m_LandingPosition = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_arrow = GameObject.Find("arrow");
-        if (!m_arrow)
-        {
-            Debug.LogError("Can't find arrow");
-        }
+        CreateLandingDisplay(); 
 
         m_soccerBall = GameObject.Find("Soccer Ball");
         if (!m_soccerBall)
@@ -49,33 +41,15 @@ public class ArrowDirection : MonoBehaviour
             Debug.LogError("Can't find soccer ball");
         }
 
-        m_arrowBox = GameObject.Find("Box");
-        if (!m_arrowBox)
-        {
-            Debug.LogError("Can't find first part of arrow");
-        }
-
-        m_arrowBox2 = GameObject.Find("Box1");
-        if (!m_arrowBox2)
-        {
-            Debug.LogError("Can't find 2nd part of arrow");
-        }
-
 
         m_qStartRotation = transform.rotation;
         m_vStartingPosition = transform.position;
 
-        m_vStartingArrowPosition = m_arrowBox.transform.position;
-        m_vStartingArrow2Position = m_arrowBox2.transform.position;
+        Debug.Log("1: " + m_vInitialVelocity);
+        m_vInitialVelocity += transform.forward * 5;
+        Debug.Log("2: " + m_vInitialVelocity);
 
-        m_vStartingArrowScale = m_arrowBox.transform.localScale;
-        m_vStartingArrow2Scale = m_arrowBox2.transform.localScale;
-
-        m_qArrowRotation = m_arrowBox.transform.rotation;
-        m_qArrow2Rotation = m_arrowBox2.transform.rotation;
-
-        m_arrowBox.GetComponent<Renderer>().material.color = Color.grey;
-        m_arrowBox2.GetComponent<Renderer>().material.color = Color.grey;
+        m_LandingPosition.transform.position += m_vInitialVelocity;
 
     }
 
@@ -84,6 +58,8 @@ public class ArrowDirection : MonoBehaviour
     {
         HandleEvents();
         Move();
+        UpdateTargetPosition();
+        RotateCamera();
 
     }
     private void HandleEvents()
@@ -99,9 +75,17 @@ public class ArrowDirection : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
-            m_bRotateUp = true;
+            m_bMoveForward = true;
         }
         if (Input.GetKeyDown(KeyCode.S))
+        {
+            m_bMoveBackwards = true;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            m_bRotateUp = true;
+        }
+        if (Input.GetKeyDown(KeyCode.F))
         {
             m_bRotateDown = true;
         }
@@ -121,24 +105,32 @@ public class ArrowDirection : MonoBehaviour
         }
         if (Input.GetKeyUp(KeyCode.W))
         {
-            m_bRotateUp = false;
+            m_bMoveForward = false;
         }
         if (Input.GetKeyUp(KeyCode.S))
+        {
+            m_bMoveBackwards = false;
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            m_bRotateUp = false;
+        }
+        if (Input.GetKeyUp(KeyCode.F))
         {
             m_bRotateDown = false;
         }
         if(Input.GetKeyUp(KeyCode.Space))
         {
             if (!m_bBallKicked)
-            { 
-            m_vDirection = new Vector3(transform.forward.x, m_arrow.transform.forward.y, transform.forward.z);
-            m_soccerBall.GetComponent<BallPhysics>().m_rb.AddForce(m_vDirection * m_fKickingStrength, ForceMode.Impulse);
-            m_bChargeKickStrength = false;
-            m_bBallKicked = true;
-            m_fKickingStrength = 1.0f;
+            {
+                 m_LandingPosition.transform.position = GetLandingPosition();
+                 m_bBallKicked = true;
+                transform.LookAt(m_LandingPosition.transform.position, Vector3.up);
+                 m_soccerBall.GetComponent<BallPhysics>().m_rb.velocity = m_vInitialVelocity;
+
             }
         }
-        if (Input.GetKeyUp(KeyCode.R))
+        if (Input.GetKeyUp(KeyCode.Q))
         {
             Debug.LogError("R Pressed");
             ResetSceneObjects();
@@ -146,23 +138,78 @@ public class ArrowDirection : MonoBehaviour
         }   
 
     }
+    
+    private void RotateCamera()
+    {
+        Vector3 m_vLandingPosDir = m_LandingPosition.transform.position - transform.position;
+        float angle = Vector3.Angle(m_vLandingPosDir, transform.forward);
+
+        float dotP = Vector3.Dot(m_vLandingPosDir, transform.right);
+        
+        if (dotP > 0)
+        {
+            transform.Rotate(0, angle, 0);
+        }
+        else
+        {
+            transform.Rotate(0, -angle, 0);
+        }
+    }
+    private void CreateLandingDisplay()
+    {
+        m_LandingPosition = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        m_LandingPosition.transform.position = transform.position + (transform.forward *5);
+        m_LandingPosition.transform.localScale = new Vector3(1.0f, 0.1f, 1.0f);
+        m_LandingPosition.GetComponent<Renderer>().material.color = Color.red;
+        m_LandingPosition.GetComponent<Collider>().enabled = false;
+    }
+
+    private void UpdateTargetPosition()
+    {
+        if (m_LandingPosition && !m_bBallKicked)
+        {
+            m_LandingPosition.transform.position = GetLandingPosition();
+        }
+    }
+
+    private Vector3 GetLandingPosition()
+    {
+        float fTime = 2f * (0f - m_vInitialVelocity.y / Physics.gravity.y);
+        Vector3 vFlatVel = m_vInitialVelocity;
+        vFlatVel.y = 0;
+        vFlatVel *= fTime;
+        Debug.LogError(fTime);
+        return transform.position + vFlatVel;
+    }
 
     private void RotateLeft(float speed)
     {
-        transform.Rotate(Vector3.up * -speed);
+        m_vInitialVelocity -= (transform.right * speed);
     }
     private void RotateRight(float speed)
     {
-        transform.Rotate(Vector3.up * speed);
+        m_vInitialVelocity += (transform.right * speed);
+    }
+    private void MoveForward(float speed)
+    {
+        m_vInitialVelocity += (transform.forward * speed);
+    }
+    private void MoveBackwards(float speed)
+    {
+        float distance = Vector3.Distance(transform.position, m_LandingPosition.transform.position);
+        if (distance > 5.0f)
+        {
+           m_vInitialVelocity -= (transform.forward * speed);
+        }
     }
     private void RotateUp(float speed)
     {
-        m_arrow.transform.Rotate(Vector3.right * -speed);
+
     }
 
     private void RotateDown(float speed)
     {
-        m_arrow.transform.Rotate(Vector3.right * speed);
+
     }
 
     private void Move()
@@ -176,6 +223,14 @@ public class ArrowDirection : MonoBehaviour
             if (m_bTurnRight)
             {
                 RotateRight(m_fRotationSpeed);
+            }
+            if (m_bMoveForward)
+            {
+                MoveForward(m_fRotationSpeed);
+            }
+            if (m_bMoveBackwards)
+            {
+                MoveBackwards(m_fRotationSpeed);
             }
             if (m_bRotateUp)
             {
@@ -194,14 +249,6 @@ public class ArrowDirection : MonoBehaviour
                 if (m_fRotationLimit < 5.0f)
                     RotateDown(m_fRotationSpeed);
             }
-
-            if (m_bChargeKickStrength)
-            {
-                Debug.LogError(m_fKickingStrength);
-                m_fKickingStrength += 0.03f;
-                m_fKickingStrength = Mathf.Clamp(m_fKickingStrength, 1.0f, 50.0f);
-                UpdateArrowSizeandColor();
-            }
         }
 
     }
@@ -212,18 +259,6 @@ public class ArrowDirection : MonoBehaviour
         m_soccerBall.transform.position = m_vStartingPosition;
         m_soccerBall.GetComponent<BallPhysics>().m_rb.velocity = Vector3.zero;
         m_soccerBall.GetComponent<BallPhysics>().m_rb.angularVelocity = Vector3.zero;
-
-        m_arrowBox.transform.localScale = m_vStartingArrowScale;
-        m_arrowBox2.transform.localScale = m_vStartingArrow2Scale;
-
-        m_arrowBox.transform.position = m_vStartingArrowPosition;
-        m_arrowBox2.transform.position = m_vStartingArrow2Position;
-
-        m_arrowBox.transform.rotation = m_qArrowRotation;
-        m_arrowBox2.transform.rotation = m_qArrow2Rotation;
-
-        m_arrowBox.GetComponent<Renderer>().material.color = Color.grey;
-        m_arrowBox2.GetComponent<Renderer>().material.color = Color.grey;
 
         ResetSceneVariables();
 
@@ -239,30 +274,6 @@ public class ArrowDirection : MonoBehaviour
         m_bBallKicked = false;
 
         m_fRotationLimit = 0.0f;
-    }
-    private void UpdateArrowSizeandColor()
-    {
-        if (!m_bBallKicked)
-        {
-            if (m_fKickingStrength < 50.0f)
-            {
-                m_arrowBox.transform.localScale += new Vector3(0.0001f, 0.0f, 0.0008f);
-                m_arrowBox2.transform.localScale += new Vector3(0.00005f, 0.0f, 0.0005f);
-                m_arrowBox.transform.position += (transform.forward * 0.0015f);
-                m_arrowBox2.transform.position += (transform.forward * 0.001f);
-            }
-
-            if (m_fKickingStrength > 20.0f && m_fKickingStrength <= 35.0f)
-            {
-                m_arrowBox.GetComponent<Renderer>().material.color = Color.yellow;
-                m_arrowBox2.GetComponent<Renderer>().material.color = Color.yellow;
-            }
-            else if (m_fKickingStrength > 35.0f)
-            {
-                m_arrowBox.GetComponent<Renderer>().material.color = Color.red;
-                m_arrowBox2.GetComponent<Renderer>().material.color = Color.red;
-            }
-        }
     }
     
     private void OnDrawGizmos()
